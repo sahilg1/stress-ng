b39df50cbface56db9eb8a967b59c8dfe65edfc7
Sahil Gupta <sahgupta@adobe.com>
2016-06-29 13:28:08 -0700

    adding other tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stress-ng
+
+docker run -e "STRESS_TEST_TYPE=CPU" -e "STRESS_WORKERS=1" -e "STRESS_CPU_PERCENTAGE=75" -d sahilg1/stress-ng-test
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -25,7 +26,34 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			print("CPU STRESS TEST")
+			fmt.Println("CPU STRESS TEST")
 		}
+	case "MEM":
+		{
+			fmt.Println("MEMORY STRESS TEST")
+			memBytes := os.Getenv("STRESS_MEM_BYTES")
+			arg1 := "-v"
+			arg2 := "--vm"
+			arg3 := "--vm-bytes"
+			cmd := exec.Command(app, arg1, arg2, Workers, arg3, memBytes)
+			err := cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}
+	case "IO":
+		{
+			fmt.Println("IO STRESS TEST")
+			arg1 := "-v"
+			arg2 := "--io"
+			cmd := exec.Command(app, arg1, arg2, Workers)
+			err := cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}
+
 	}
 }
